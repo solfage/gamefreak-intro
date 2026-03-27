51322882ca017875430bb3f9f86e8afce904ceea
solfage <solfage777@gmail.com>
2026-03-27 06:34:36 +0200

    Add TopFlame admin dashboard controller

---
+++ topflame/app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+
+class DashboardController extends Controller
+{
+    public function index(): View
+    {
+        return view('admin.dashboard', [
+            'pageTitle' => 'Admin Dashboard',
+        ]);
+    }
+}
