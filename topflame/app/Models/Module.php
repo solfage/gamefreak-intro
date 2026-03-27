fe90ca2c1a45332af934ecac793d9d445f37d43e
solfage <solfage777@gmail.com>
2026-03-27 06:57:26 +0200

    Add TopFlame module model

---
+++ topflame/app/Models/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Module extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'version',
+        'target_version',
+        'is_enabled',
+        'manifest',
+    ];
+
+    protected $casts = [
+        'is_enabled' => 'boolean',
+        'manifest' => 'array',
+    ];
+}
